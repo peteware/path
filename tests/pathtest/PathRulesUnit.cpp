c713e51ee305bc2850655120737ade4f9fe639d5
ware <ware@peteware.com>
2007-10-04 08:14:11 -0400

    Got PathRulesUnit compiling

--- tests/pathtest/PathRulesUnit.cpp
+++ tests/pathtest/PathRulesUnit.cpp
@@ -9,6 +9,8 @@ class PathRulesUnit: public CppUnit::TestCase
 	CPPUNIT_TEST_SUITE(PathRulesUnit);
 
 	CPPUNIT_TEST(init);
+
+	CPPUNIT_TEST_SUITE_END();
 protected:
 	/// Make sure constructors/destructor works
 	void init();
@@ -18,5 +20,6 @@ CPPUNIT_TEST_SUITE_REGISTRATION(PathRulesUnit);
 
 void PathRulesUnit::init()
 {
-	PathRulesUnix	unix_rules;
+	UnixRules	unix_rules;
 }
+
