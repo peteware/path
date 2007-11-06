0d90992228d27159eda43fbd7d7bc72358a08c63
ware <ware@peteware.com>
2007-11-06 07:43:12 -0500

    Added Node unit tests

---
+++ tests/pathtest/NodeUnit.cpp
@@ -0,0 +1,43 @@
+#include <Node.h>
+#include <NodeIter.h>
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class NodeUnit: public CppUnit::TestCase
+{
+	CPPUNIT_TEST_SUITE(NodeUnit);
+
+	CPPUNIT_TEST(init);
+	CPPUNIT_TEST(iter);
+
+	CPPUNIT_TEST_SUITE_END();
+protected:
+	/// Test basic initialization
+	void init();
+	/// Test node iteration
+	void iter();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(NodeUnit);
+
+void NodeUnit::init()
+{
+	Node	node (".");
+	Node	empty;
+}
+
+void NodeUnit::iter()
+{
+	Node	node(".");
+	int		count = 0;
+
+	for (Node::iterator iter = node.begin(); iter != node.end(); ++iter)
+	{
+		std::cout << (*iter).str() << " ";
+		++count;
+	}
+	if (count > 0)
+		std::cout << std::endl;
+	
+}
