b4657f771ed870250dc3cc0561ad061ee1bd84ad
ware <ware@peteware.com>
2008-04-01 16:50:46 -0400

    New test program

--- SConstruct
+++ SConstruct
@@ -1,6 +1,6 @@
 # -*- python -*-
 import os
-for dir in ['src', os.path.join('tests', 'pathtest')]:
+for dir in ['src', os.path.join('tests', 'pathtest'), os.path.join('tests', 'search')]:
     SConscript(dirs = dir, 
            build_dir = os.path.join('build', 'Debug', dir), duplicate = 0)
 #'tests/pathtest/SConscrtipt',
---
+++ tests/search/main.cpp
@@ -0,0 +1,60 @@
+#include <path/Node.h>
+#include <path/NodeIter.h>
+#include <path/Canonical.h>
+#include <path/PathException.h>
+
+#include <getopt.h>
+#include <iostream>
+
+extern char *optarg;
+extern int optind;
+extern int optopt;
+extern int opterr;
+extern int optreset;
+
+int main(int argc, char **argv)
+{
+	int		ch;		// Input option
+	size_t	size;	// Size of the file to search for
+	char *	name;	// Name of the file to search
+	/* options descriptor */
+	static struct option longopts[] = {
+		{ "size",		required_argument,      NULL,           's' },
+		{ "name",   	required_argument,      NULL,           'n' },
+		{ NULL,         0,                      NULL,           0 }
+	};
+
+	while ((ch = getopt_long(argc, argv, "s:n:", longopts, NULL)) != -1)
+	{
+		switch (ch)
+		{
+		case 's':
+			size = atoi(optarg);
+			break;
+		case 'n':
+			name = optarg;
+			break;
+		default:
+			exit(1);
+			break;
+		}
+	}
+	
+	for (int i = optind; i < argc; ++i)
+	{
+        try
+        {
+            Node	node (Path(UnixPath(argv[i])));
+            std::cout << node.path() << std::endl;
+            for (Node::iterator iter = node.begin(); iter != node.end(); ++iter)
+            {
+                std::cout << iter->path() << std::endl;
+            }
+        }
+        catch (PathException ex)
+        {
+			std::cerr << ex.what() << std::endl;
+        }
+	}
+	
+}
