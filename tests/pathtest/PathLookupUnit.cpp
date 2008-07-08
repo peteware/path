3783ae6ca28f6cc75e6ed9ca5e874ea338689dfb
Pete Ware <ware@peteware.com>
2008-07-08 09:08:45 -0400

    Added PathLookupUnit test

---
+++ tests/pathtest/PathLookupUnit.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file PathLookupUnit.cpp
+ * @ingroup PathTest
+ */
+#include <path/PathLookup.h>
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace path;
+
+/**
+ * Implements unit tests for PathLookup class
+ *
+ */ 
+class PathLookupUnit : public CppUnit::TestCase
+{
+	CPPUNIT_TEST_SUITE(PathLookupUnit);
+    
+	CPPUNIT_TEST(init);
+    
+	CPPUNIT_TEST_SUITE_END();
+    
+protected:
+	/// Test constructor
+    void init();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(PathLookupUnit);
+
+void PathLookupUnit::init()
+{
+    PathLookup look;
+    CPPUNIT_ASSERT_EQUAL(Path(), look.find("abc"));
+}
